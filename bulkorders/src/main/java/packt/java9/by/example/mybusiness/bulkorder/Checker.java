04df35316d72a58eceb55060d698dbe5beffd1e8
Peter Verhas <peter@verhas.com>
2016-11-20 08:19:52 +0100

    .gitignore added

---
+++ bulkorders/src/main/java/packt/java9/by/example/mybusiness/bulkorder/Checker.java
@@ -0,0 +1,7 @@
+package packt.java9.by.example.mybusiness.bulkorder;
+
+/**
+ * Created by verhasp on 2016. 11. 19..
+ */
+public class Checker {
+}
---
+++ bulkorders/src/main/java/packt/java9/by/example/mybusiness/bulkorder/checkers/Tuple.java
@@ -0,0 +1,7 @@
+package packt.java9.by.example.mybusiness.bulkorder.checkers;
+
+/**
+ * Created by verhasp on 2016. 11. 20..
+ */
+public class Tuple {
+}
