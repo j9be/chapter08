350d8e00c92161abd1f8df9d4907f261b132e6b1
Peter Verhas <peter@verhas.com>
2016-11-20 12:26:19 +0100

    CheckHelper refactored to streams

--- bulkorders/src/main/java/packt/java9/by/example/mybusiness/bulkorder/checkers/CheckHelper.java
+++ bulkorders/src/main/java/packt/java9/by/example/mybusiness/bulkorder/checkers/CheckHelper.java
@@ -1,16 +1,25 @@
 package packt.java9.by.example.mybusiness.bulkorder.checkers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import packt.java9.by.example.mybusiness.bulkorder.pobeans.Order;
 import packt.java9.by.example.mybusiness.bulkorder.pobeans.OrderItem;
 
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static packt.java9.by.example.mybusiness.bulkorder.checkers.Tuple.tuple;
+
 public class CheckHelper {
+    private static final Logger log = LoggerFactory.getLogger(CheckHelper.class);
     final Order order;
 
     public CheckHelper(Order order) {
         this.order = order;
     }
 
-    public boolean containsOneOf(String... ids) {
+    public boolean _containsOneOf(String... ids) {
         for (final OrderItem item : order.getItems()) {
             for (final String id : ids) {
                 if (item.getProductId().equals(id)) {
@@ -21,4 +30,11 @@ public class CheckHelper {
         return false;
     }
 
+    public boolean containsOneOf(String... ids) {
+        return order.getItems().stream()
+                .map( item -> item.getProductId())
+                .flatMap(itemId -> Arrays.stream(ids).map(id -> tuple(itemId, id)))
+                .filter(t -> Objects.equals(t.s, t.r))
+                .collect(Collectors.toSet()).size() > 0;
+    }
 }
